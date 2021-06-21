dd0ef8d0fa97e0040e8f6eda1e02c7c6df3f623a
xOS <git@nange.cn>
2021-06-22 04:02:05 +0800

    Update

--- iputil/geo/geo.go
+++ iputil/geo/geo.go
@@ -57,7 +57,7 @@ type geoip struct {
 	connectiontype     *geoip2.Reader
 }
 
-func Open(countryDB, cityDB string, asnDB string, ispDB string, connectiontypeDB string) (Reader, error) {
+func Open(countryDB string, cityDB string, asnDB string, ispDB string, connectiontypeDB string) (Reader, error) {
 	var country, city, asn, isp, connectiontype *geoip2.Reader
 	if countryDB != "" {
 		r, err := geoip2.Open(countryDB)
