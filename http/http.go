f4d42169e0767fd701ad6471771c32b22d4b123a
xOS <git@nange.cn>
2021-06-22 22:40:29 +0800

    Update.

--- http/http.go
+++ http/http.go
@@ -53,7 +53,7 @@ type Response struct {
 	Timezone    string               `json:"time_zone,omitempty"`
 	ASN         string               `json:"asn,omitempty"`
 	ISP         string               `json:"isp,omitempty"`
-	ASOrg       string               `json:"asn_org,omitempty"`
+	ASO         string               `json:"asn_org,omitempty"`
 	Org         string               `json:"isp_org,omitempty"`
 	Hostname    string               `json:"hostname,omitempty"`
 	UserAgent   *useragent.UserAgent `json:"user_agent,omitempty"`
@@ -164,7 +164,7 @@ func (s *Server) newResponse(r *http.Request) (Response, error) {
 		Timezone:       city.Timezone,
 		ASN:            autonomousSystemNumber,
 		ISP:            isp.ISP,
-		ASOrg:          isp.AutonomousSystemOrganization,
+		ASO:            isp.AutonomousSystemOrganization,
 		Org:            isp.Organization,
 		Hostname:       hostname,
 	}
