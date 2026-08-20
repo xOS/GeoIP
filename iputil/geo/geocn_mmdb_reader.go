5fe34487c540512734ea1039c8a92d716e5cf28a
春夏 <git@aapls.com>
2026-08-20 12:46:35 +0800

    Bug fix.

--- iputil/geo/geocn_mmdb_reader.go
+++ iputil/geo/geocn_mmdb_reader.go
@@ -18,17 +18,51 @@ type GeoCNRecord struct {
 		ISOCode string            `maxminddb:"iso_code"`
 		Names   map[string]string `maxminddb:"names"`
 	} `maxminddb:"country"`
-	Province  string `maxminddb:"province"`
-	City      string `maxminddb:"city"`
-	Districts string `maxminddb:"districts"`
-	ISP       string `maxminddb:"isp"`
-	Net       string `maxminddb:"net"`
-	Location  struct {
+	Province    interface{} `maxminddb:"province"`
+	Subdivision interface{} `maxminddb:"subdivision"`
+	City        interface{} `maxminddb:"city"`
+	Districts   interface{} `maxminddb:"districts"`
+	District    interface{} `maxminddb:"district"`
+	County      interface{} `maxminddb:"county"`
+	Area        interface{} `maxminddb:"area"`
+	ISP         interface{} `maxminddb:"isp"`
+	Net         interface{} `maxminddb:"net"`
+	GeoCN       struct {
+		Division struct {
+			Full  []string `maxminddb:"full"`
+			Short []string `maxminddb:"short"`
+		} `maxminddb:"division"`
+		DivisionCode interface{} `maxminddb:"division_code"`
+		ISP          interface{} `maxminddb:"isp"`
+	} `maxminddb:"geo_cn"`
+	Location struct {
 		Latitude  float64 `maxminddb:"latitude"`
 		Longitude float64 `maxminddb:"longitude"`
 		TimeZone  string  `maxminddb:"time_zone"`
 	} `maxminddb:"location"`
 	Network string `maxminddb:"network"`
+}
+
+// extractGeoString robustly extracts a string from an interface{} which might be a string or a slice
+func extractGeoString(val interface{}) string {
+	if val == nil {
+		return ""
+	}
+	switch v := val.(type) {
+	case string:
+		return v
+	case []interface{}:
+		if len(v) > 0 {
+			if s, ok := v[0].(string); ok {
+				return s
+			}
+		}
+	case []string:
+		if len(v) > 0 {
+			return v[0]
+		}
+	}
+	return ""
 }
 
 // NewGeoCNMMDBReader creates a new GeoCN MMDB reader
@@ -87,11 +121,36 @@ func (g *GeoCNMMDBReader) City(ip net.IP) (City, error) {
 	}
 
 	city := City{}
-	city.Name = record.City
-	// 根据要求，province对应region
-	city.RegionName = record.Province
-	// 根据要求，districts对应district
-	city.District = record.Districts
+	city.Name = extractGeoString(record.City)
+	if city.Name == "" && len(record.GeoCN.Division.Full) >= 2 {
+		city.Name = record.GeoCN.Division.Full[1]
+	}
+
+	// province
+	prov := extractGeoString(record.Province)
+	if prov == "" {
+		prov = extractGeoString(record.Subdivision)
+	}
+	if prov == "" && len(record.GeoCN.Division.Full) >= 1 {
+		prov = record.GeoCN.Division.Full[0]
+	}
+	city.RegionName = prov
+
+	// district
+	dist := extractGeoString(record.Districts)
+	if dist == "" {
+		dist = extractGeoString(record.District)
+	}
+	if dist == "" {
+		dist = extractGeoString(record.County)
+	}
+	if dist == "" {
+		dist = extractGeoString(record.Area)
+	}
+	if dist == "" && len(record.GeoCN.Division.Full) >= 3 {
+		dist = record.GeoCN.Division.Full[2]
+	}
+	city.District = dist
 
 	// Set coordinates if available
 	if record.Location.Latitude != 0 || record.Location.Longitude != 0 {
@@ -128,11 +187,15 @@ func (g *GeoCNMMDBReader) ISP(ip net.IP) (ISP, error) {
 	}
 
 	isp := ISP{}
-	isp.ISP = record.ISP
+	ispStr := extractGeoString(record.ISP)
+	if ispStr == "" {
+		ispStr = extractGeoString(record.GeoCN.ISP)
+	}
+	isp.ISP = ispStr
 
 	// If we have ISP info, also set the organization
-	if record.ISP != "" {
-		isp.Organization = record.ISP
+	if ispStr != "" {
+		isp.Organization = ispStr
 	}
 
 	return isp, nil
@@ -153,7 +216,7 @@ func (g *GeoCNMMDBReader) ConnectionType(ip net.IP) (ConnectionType, error) {
 
 	connType := ConnectionType{}
 	// 根据要求，net对应ConnectionType
-	connType.ConnectionType = record.Net
+	connType.ConnectionType = extractGeoString(record.Net)
 
 	return connType, nil
 }
